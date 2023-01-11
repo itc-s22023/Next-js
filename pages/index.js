0da1daeba5f6e8e0f17b2b4ca96aa2e1fb483ef8
itc-s22023 <s22023@std.it-college.ac.jp>
2023-01-11 08:03:05 +0000

    1111703

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home(){
 	return(
-		<Hero
-		title="CUBE"
-		subtitle="アウトプットしていくサイトaa"
-		imageOn
-		/>
+		<Container>
+	<Hero title="CUBE"subtitle="アウトプットしていくサイト" imageOn/>
+	</Container>
 	)
 
 }
